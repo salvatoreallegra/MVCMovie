7620394f40f8d7a9bb608b6050e0803e2ccf3df3
Salvatore Allegra <salallegra@gmail.com>
2025-03-16 17:34:42 -0700

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is the default Action, /HelloWorld";
+        }
+        public string Welcome()
+        {
+            return "This is the Welcome Action Method";
+        }
+    }
+}
